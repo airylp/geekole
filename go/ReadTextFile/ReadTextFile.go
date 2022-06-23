c6388330556e4f7068c4289ae9f44ae1b9aff61b
Ant <airylp@gmail.com>
2022-06-22 20:48:06 -0500

    Read a text file

---
+++ go/ReadTextFile/ReadTextFile.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("example.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
