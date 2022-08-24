5502598cb829a6b6987a56216544e8e9ac7c033c
Ant <airylp@gmail.com>
2022-08-24 14:22:06 -0500

    The Longest Substring

--- go/exercises/LongestSubstring/LongestSubstring.go
+++ go/exercises/LongestSubstring/LongestSubstring.go
@@ -3,15 +3,15 @@ package main
 import "fmt"
 
 func lengthOfLongestSubstring(s string) int {
-	m := make(map[string]int, len(s))
+	m := make(map[byte]int, len(s))
 
 	l := 0
 	if len(s) > 0 {
 		for i := 0; i < len(s); i++ {
-			m[string(s[i])] = i
+			m[s[i]] = i
 			for j := i + 1; j < len(s); j++ {
 				//fmt.Println(string(s[i]), string(s[j]))
-				if _, exist := m[string(s[j])]; exist {
+				if _, exist := m[s[j]]; exist {
 					//fmt.Println(string(s[j]))
 					if len(m) > l {
 						l = len(m)
@@ -21,7 +21,7 @@ func lengthOfLongestSubstring(s string) int {
 					}
 					break
 				} else {
-					m[string(s[j])] = j
+					m[s[j]] = j
 				}
 			}
 		}
