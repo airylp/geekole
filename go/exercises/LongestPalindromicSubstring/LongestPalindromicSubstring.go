fc77ba272ac6a7580a0d8559a407a1bbeb5bd767
Ant <airylp@gmail.com>
2022-09-13 12:30:38 -0500

    LongestPalindromicSubstring

--- go/exercises/LongestPalindromicSubstring/LongestPalindromicSubstring.go
+++ go/exercises/LongestPalindromicSubstring/LongestPalindromicSubstring.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func longestPalindrome(s string) string {
 	palInit := 0
 	PalEnd := 0
@@ -38,5 +40,5 @@ func longestPalindrome(s string) string {
 }
 
 func main() {
-	//fmt.Println("res: ", longestPalindrome("12345654321abaaba"))
+	fmt.Println("res: ", longestPalindrome("12345654321abaaba"))
 }
